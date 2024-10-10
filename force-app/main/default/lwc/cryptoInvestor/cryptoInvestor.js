f6187d41849d9d54616ed9df9f8bc2b3d70e9fe3
StepanZapotichnyi <125075465+StepanZapotichnyi@users.noreply.github.com>
2024-10-10 15:38:29 +0200

    created tab history, add new modal and some changes

--- force-app/main/default/lwc/cryptoInvestor/cryptoInvestor.js
+++ force-app/main/default/lwc/cryptoInvestor/cryptoInvestor.js
@@ -2,18 +2,22 @@ import { LightningElement, track } from 'lwc';
 import { ShowToastEvent} from 'lightning/platformShowToastEvent';
 import getInvestmentDetails from '@salesforce/apex/CryptoInvestorController.getInvestmentDetails';
 import getDataTableDetails from '@salesforce/apex/CryptoInvestorController.getDataTableDetails';
+// import getDateTimeNow from '@salesforce/apex/CryptoInvestorController.getDateTimeNow';
 import CreatePortfolioModal from 'c/createPortfolioModal';
 import createPortfolio from '@salesforce/apex/PortfolioController.createPortfolio';
 import deletePortfolio from '@salesforce/apex/PortfolioController.deletePortfolio';
 import createTransaction from '@salesforce/apex/TransactionController.createTransaction';
 import CreateTransactionModal from 'c/createTransactionModal';
 import BuyOrSellModal  from 'c/buyOrSellModal';
+import SettingTheTimeRangeModal  from 'c/settingTheTimeRangeModal';
+import StartDate from '@salesforce/schema/Contract.StartDate';
+
 
 const ACTIONS = [
     { label: 'Buy', name: 'buy_token' },
     { label: 'Sell', name: 'sell_token' },
 ];
-const COLUMNS = [
+const COLUMNS_TO_PORTFOLIO = [
     {label: 'Name', fieldName : 'name'},
     {label: 'Price', fieldName : 'price'},
     {label: 'Holdings', fieldName : 'holdings'},
@@ -25,6 +29,17 @@ const COLUMNS = [
         type: 'action',
         typeAttributes: { rowActions: ACTIONS },
     },
+];
+
+const COLUMNS_TO_HISTORY_TRANSACTIONS = [
+    {label: 'Symbol', fieldName : 'symbol'},
+    {label: 'Name', fieldName : 'name'},
+    {label: 'Price Per Coin', fieldName : 'price_per_coin'},
+    {label: 'Quantity', fieldName : 'quantity'},
+    {label: 'Spent', fieldName : 'spending'},
+    {label: 'Type', fieldName : 'typeTransaction'},
+    {label: 'Created Date', fieldName : 'created_date'},
+    
 ];
 
 const PAGE_SIZE = 10;
@@ -40,20 +55,59 @@ export default class CryptoInvestor extends LightningElement {
     @track selectedPortfolio = {};
     @track portfolios = [];
     @track dataTable = [];
-    priceList= [];
+    @track transactions = [];
+    interimToDataTablePortfolio = [];
     
-    columns = COLUMNS;
+    columnsToPortfolio = COLUMNS_TO_PORTFOLIO;
 
     
     isLoading = false;
     isMenuOpen = false;
-    @track isPaginator = false;
+    @track isPaginatorToPortfolioTab = false;
     
-    @track pageNumber = 1;
-    @track numberOfPages;
-    startingPageIndex = 1;
-    endingPageIndex = 0;
-    totalRecordCount;
+    @track pageNumberToPortfolioTab = 1;
+    @track numberOfPagesToPortfolioTab;
+    startingPageIndexToPortfolioTab = 1;
+    endingPageIndexToPortfolioTab = 0;
+    totalRecordCountToPortfolioTab;
+
+
+    isPaginatorToHistoryTransactions = false;
+    @track pageNumberToHistoryTab = 1;
+    @track numberOfPagesToHistoryTab;
+
+    startingPageIndexToHistoryTab = 1;
+    endingPageIndexToHistoryTab = 0; 
+    totalRecordCountToHistoryTab;
+    @track dataTableToHistoryTab =[];
+
+
+
+    
+
+    @track transactionTypeOptions = [
+        { key: 'all_type', value: 'All' },
+        { key: 'buy_type', value: 'Buy' },
+        { key: 'sell_type', value: 'Sell' },
+    ];
+
+    @track transactionTimeOptions = [
+        { key: 'all_time', value: 'All The Time' },
+        { key: '7days', value: 'Last 7 days' },
+        { key: '30days', value: 'Last 30 days' },
+        { key: '90days', value: 'Last 90 days' },
+        { key: 'setting', value: 'Set the date' },
+    ];
+
+    startDate = {};
+    endDate = {};
+    keyTransactionTypeOption = '';
+    keyTransactionTimeOption = '';
+    symbolTransaction = '';
+    transactionsHistoryData = [];
+    columnsToHistoryTransactions = COLUMNS_TO_HISTORY_TRANSACTIONS;
+
+
     
     connectedCallback(){
         this.loadDetails();    
@@ -63,9 +117,12 @@ export default class CryptoInvestor extends LightningElement {
         this.isLoading = true;
         getInvestmentDetails({})
         .then(result => {
-            this.amountInvestments = `${result.totalBalanceInvestment} ${SYMBOL_USD}`;
-            this.currencyBalance = `${result.currencyBalance} ${SYMBOL_USD}`;
+            this.amountInvestments = `${result.totalBalanceInvestment}${SYMBOL_USD}`;
+            this.currencyBalance = `${result.currencyBalance}${SYMBOL_USD}`;
             this.portfolios = result.portfolios;
+            this.transactions = result.transactions;
+            this.formatTransactionHistoryData(this.transactions);
+
             
         })
         .catch(error => {
@@ -147,13 +204,12 @@ export default class CryptoInvestor extends LightningElement {
         await getDataTableDetails({ portfolioId: portfolioId })
             .then(result => {
 
-                 this.priceList = this.createdPriceListToDataTable(result);
+                 this.interimToDataTablePortfolio = this.createdInterimToDataTablePortfolio(result);
  
-                this.totalRecordCount = this.priceList.length;
-                this.numberOfPages = Math.ceil(this.totalRecordCount / PAGE_SIZE);
-                this.displayRecordPerPage(this.pageNumber);  
-
-                this.calculateAverageProfitAndLoss(this.priceList);
+                this.totalRecordCountToPortfolioTab = this.interimToDataTablePortfolio.length;
+                this.numberOfPagesToPortfolioTab = Math.ceil(this.totalRecordCountToPortfolioTab / PAGE_SIZE);
+                this.displayRecordPerPageToPortfolioTab(this.pageNumberToPortfolioTab);
+                this.calculateAverageProfitAndLoss(this.interimToDataTablePortfolio);
             })
             .catch(error => {
                 console.error('Error loading dataTable:', error);
@@ -161,7 +217,7 @@ export default class CryptoInvestor extends LightningElement {
 
     }
     
-    createdPriceListToDataTable(data) {
+    createdInterimToDataTablePortfolio(data) {
         console.log(JSON.stringify(data))
         return data.map(item => {
             let totalQuantity = parseFloat(item.totalQuantity) || 0; 
@@ -177,8 +233,35 @@ export default class CryptoInvestor extends LightningElement {
                 profitAndLoss: this.calculateProfitAndLoss(item), 
             };
         });
+    }
 
+
+    displayRecordPerPageToPortfolioTab(page) {
+        page = Math.max(1, Math.min(page, this.numberOfPagesToPortfolioTab));
+
+        this.pageNumberToPortfolioTab = page;
+    
+        this.startingPageIndexToPortfolioTab = parseFloat(page - 1) * PAGE_SIZE;
+        this.endingPageIndexToPortfolioTab = Math.min(page * PAGE_SIZE, this.interimToDataTablePortfolio.length);
+
+        this.dataTable = this.interimToDataTablePortfolio.slice(this.startingPageIndexToPortfolioTab, this.endingPageIndexToPortfolioTab);
+    
+        this.isPaginatorToPortfolioTab = this.interimToDataTablePortfolio.length >= (parseFloat(PAGE_SIZE+1));
         
+    }
+    
+    prevHandlerToPortfolioTab(event) {
+        if (this.pageNumberToPortfolioToPortfolioTab > 1) {
+            this.pageNumberToPortfolioToPortfolioTab -= 1;
+            this.displayRecordPerPageToPortfolioTab(this.pageNumberToPortfolioTab);
+        }
+    }
+
+    nextHandlerToPortfolioTab(event) {
+        if (this.pageNumberToPortfolioTab < this.numberOfPagesToPortfolioTab) {
+            this.pageNumberToPortfolioTab += 1;
+            this.displayRecordPerPageToPortfolioTab(this.pageNumberToPortfolioTab);
+        }
     }
 
     calculateProfitAndLoss(item) {
@@ -196,8 +279,8 @@ export default class CryptoInvestor extends LightningElement {
     }
     
 
-    calculateAverageProfitAndLoss(priceList) {
-        let profitAndLossValues = priceList.map(item => parseFloat(item.profitAndLoss.replace(/\$/g, '')));
+    calculateAverageProfitAndLoss(interimToDataTablePortfolio) {
+        let profitAndLossValues = interimToDataTablePortfolio.map(item => parseFloat(item.profitAndLoss.replace(/\$/g, '')));
         this.averageProfitAndLoss = profitAndLossValues.reduce((total, value) => total + value, 0);
         this.averageProfitAndLoss = this.averageProfitAndLoss.toFixed(4);
 
@@ -230,36 +313,7 @@ export default class CryptoInvestor extends LightningElement {
 
     }
 
-    displayRecordPerPage(page) {
-    
-        page = Math.max(1, Math.min(page, this.numberOfPages));
-
-        this.pageNumber = page;
-    
-        this.startingPageIndex = parseFloat(page - 1) * PAGE_SIZE;
-        this.endingPageIndex = Math.min(page * PAGE_SIZE, this.priceList.length);
-
-        this.dataTable = this.priceList.slice(this.startingPageIndex, this.endingPageIndex);
-    
-        this.isPaginator = this.priceList.length >= (parseFloat(PAGE_SIZE+1));
-        
-    }
-    
-
-    
-    prevHandler(event) {
-        if (this.pageNumber > 1) {
-            this.pageNumber -= 1;
-            this.displayRecordPerPage(this.pageNumber);
-        }
-    }
-
-    nextHandler(event) {
-        if (this.pageNumber < this.numberOfPages) {
-            this.pageNumber += 1;
-            this.displayRecordPerPage(this.pageNumber);
-        }
-    }
+   
 
     updatePortfolioHighlight(portfolioId) {
     
@@ -312,6 +366,7 @@ export default class CryptoInvestor extends LightningElement {
 
         createTransaction({data: JSON.stringify(transactionData)})
         .then(response => {
+            // this.loadDetails();
             this.handleSelectPortfolio({currentTarget: {dataset: {id: this.selectedPortfolio.Id}}});
             this.showToast('Success', 'Transaction completed', 'success');
         })
@@ -402,7 +457,173 @@ export default class CryptoInvestor extends LightningElement {
         });
 
     }
+ 
 
+    formatTransactionHistoryData(transactions) {
+        this.transactionsHistoryData = transactions.map(item => ({
+            id: item.Id,
+            symbol: item.Symbol__c,
+            name: item.Name,
+            price_per_coin: `${SYMBOL_USD}${item.Price_Per_Coin__c}`,
+            quantity: item.Quantity__c,
+            spending: `${SYMBOL_USD}${item.Amount__c}`,
+            typeTransaction: item.Type_Transaction__c,
+            created_date: item.CreatedDate.split('T')[0],
+        }));
+
+        this.toggleToShowEmptyDataMessage();
+        this.totalRecordCountToHistoryTab = this.transactionsHistoryData.length;
+        this.numberOfPagesToHistoryTab = Math.ceil(this.totalRecordCountToHistoryTab / PAGE_SIZE);
+    
+        this.displayRecordPerPageToHistoryTab(this.pageNumberToHistoryTab);
+
+    }
+
+    toggleToShowEmptyDataMessage() {
+        const emptyDataTable = this.template.querySelector('.empty-datatable');
+    
+        if(emptyDataTable){
+            if (emptyDataTable) {
+                const hasData = this.transactionsHistoryData && this.transactionsHistoryData.length > 0;
+                emptyDataTable.classList.toggle('slds-hide', hasData);
+            }
+        }
+    }
+
+    displayRecordPerPageToHistoryTab(page) {
+
+        page = Math.max(1, Math.min(page, this.numberOfPagesToHistoryTab));
+
+        this.pageNumberToHistoryTab = page;
+    
+        this.startingPageIndexToHistoryTab = parseFloat(page - 1) * PAGE_SIZE;
+        this.endingPageIndexToHistoryTab = Math.min(page * PAGE_SIZE, this.transactionsHistoryData.length);
+
+        this.dataTableToHistoryTab = this.transactionsHistoryData.slice(this.startingPageIndexToHistoryTab, this.endingPageIndexToHistoryTab);
+    
+        this.isPaginatorToHistoryTransactions = this.transactionsHistoryData.length >= (parseFloat(PAGE_SIZE+1));
+        
+    }
+
+
+    nextHandlerToHistoryTab(event){
+        if (this.pageNumberToHistoryTab < this.numberOfPagesToHistoryTab) {
+            this.pageNumberToHistoryTab += 1;
+            this.displayRecordPerPageToHistoryTab(this.pageNumberToHistoryTab);
+        }
+    }
+
+    prevHandlerToHistoryTab(event) {
+        if (this.pageNumberToHistoryTab > 1) {
+            this.pageNumberToHistoryTab -= 1;
+            this.displayRecordPerPageToHistoryTab(this.pageNumberToHistoryTab);
+        }
+    }
+
+
+    async handleChangeTime(event) {
+        this.keyTransactionTimeOption = event.target.value;
+        this.isValueChangeTime(this.keyTransactionTimeOption);
+    }
+
+    isValueChangeTime(keyTransactionTimeOption) {
+        if (keyTransactionTimeOption === 'all_time') {
+            this.keyTransactionTimeOption = '';
+            this.filterTransactionData();
+        } else if (keyTransactionTimeOption === 'setting') {
+            this.openDateRangeModal();
+        } else {
+            this.setDate(keyTransactionTimeOption);
+            this.filterTransactionData();
+        }
+    }
+
+    async openDateRangeModal() {
+        const customDateRangeTransactions = await SettingTheTimeRangeModal.open({});
+        if (customDateRangeTransactions) {
+            this.startDate = new Date(customDateRangeTransactions.startDate);
+            this.endDate = new Date(customDateRangeTransactions.endDate);
+            this.filterTransactionData();
+        }
+    }
+
+    setDate(keyTransactionTimeOption) {
+        const daysMap = {
+            '7days': 7,
+            '30days': 30,
+            '90days': 90,
+        };
+        const amountOfDays = daysMap[keyTransactionTimeOption];
+        if(!amountOfDays) {
+            return this.showToast('Error', `Date not set`, 'error');;
+        }
+        
+        this.startDate = new Date(); 
+        this.endDate = new Date(this.startDate);
+        this.endDate.setDate(this.startDate.getDate() - amountOfDays);
+    }
+
+    handleSearchSymbol(event) {
+        this.symbolTransaction = event.target.value.toUpperCase();
+        if (this.symbolTransaction) {
+            this.filterTransactionData();
+        }
+    }
+
+
+    handleChangeType(event) {
+        this.typeTransaction = event.target.value;
+        if (this.typeTransaction === 'buy_type') {
+            this.typeTransaction = 'Buy';
+        } else if (this.typeTransaction === 'sell_type') {
+            this.typeTransaction = 'Sell';
+        }else if(this.typeTransaction === 'all_type'){
+            this.typeTransaction = '';
+        }
+        this.filterTransactionData();
+    }
+
+    filterTransactionData() {
+        let interimFilteredData = [...this.transactions];
+
+        if (this.symbolTransaction) {
+           interimFilteredData = interimFilteredData.filter(transaction => transaction.Symbol__c === this.symbolTransaction);
+        }
+
+        if (this.typeTransaction) {
+           interimFilteredData = interimFilteredData.filter(transaction => transaction.Type_Transaction__c === this.typeTransaction);
+        }
+
+        if (this.keyTransactionTimeOption && this.startDate && this.endDate) {
+           interimFilteredData = interimFilteredData.filter(transaction => {
+                const transactionDate = new Date(transaction.CreatedDate);
+                return transactionDate >= this.endDate && transactionDate <= this.startDate;
+            });
+        }
+
+        this.formatTransactionHistoryData(interimFilteredData);
+    }
+
+    handleReset() {
+        this.formatTransactionHistoryData(this.transactions);
+        this.cleanFilter();
+
+        const timeSelect = this.template.querySelector('[data-id="time-select"]');
+        const typeSelect = this.template.querySelector('[data-id="type-select"]');
+        const symbolInput = this.template.querySelector('[data-id="symbol-input"]');
+
+        if (timeSelect) timeSelect.value = 'all_time'; 
+        if (typeSelect) typeSelect.value = 'all_type'; 
+        if (symbolInput) symbolInput.value = ''; 
+    }
+
+    cleanFilter() {
+        this.startDate = {};
+        this.endDate = {};
+        this.keyTransactionTypeOption = '';
+        this.keyTransactionTimeOption = '';
+        this.symbolTransaction = '';
+    }
 
     showToast(title, message, variant) {
         const evt = new ShowToastEvent({
